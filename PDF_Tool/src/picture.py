92f9397f8d0945cf6ed69652da2e9eff83e16cd2
Graf-J <johannes.graf.1908@gmail.com>
2021-05-20 19:50:43 +0200

    Simplified Picture Class

--- PDF_Tool/src/picture.py
+++ PDF_Tool/src/picture.py
@@ -1,15 +1,10 @@
 from PIL import Image
-from fpdf import FPDF
 
 class Picture:
     def __init__(self, path):
         self.picture = Image.open(path)
-        self.path = path
         self.file_name = path.split('/')[-1].split('.')[0]
 
     def convertToPdf(self, out):
-        width, height = self.picture.size
-        pdf = FPDF(unit='pt', format=[width, height])
-        pdf.add_page()
-        pdf.image(self.path, 0, 0)
-        pdf.output(f'{ out }/{ self.file_name }.pdf', 'F')
+        self.picture.convert('RGB')
+        self.picture.save(f'{ out }/{ self.file_name }.pdf')
